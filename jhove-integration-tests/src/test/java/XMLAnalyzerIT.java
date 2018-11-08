b8dd46ef4db5d18beabf225d3dec9fe9d7216ed3
Dave Gerrard <dg509@cam.ac.uk>
2018-11-08 13:37:18 +0000

    Injects integration testing point and project into Maven build config.

---
+++ jhove-integration-tests/src/test/java/XMLAnalyzerIT.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class XMLAnalyzerIT {
+
+    @Test
+    public void testXMLAnalyzer() {
+        assertEquals(1, 1);
+    }
+
+}
